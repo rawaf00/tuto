22cfbebc4c9d13050b98a8abc97f6f2fc8f167fd
rawaf <mr.rar10@gmail.com>
2022-10-27 13:45:53 +0300

    firs

---
+++ Text_To_Speech/Text_To_Speech/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Text_To_Speech
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Text_To_Speech/Text_To_Speech/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Plugin.TextToSpeech;
+
+namespace Text_To_Speech
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            TextToSpeech.SpeakAsync("hello ");
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            TextToSpeech.SpeakAsync(MainEntry.Text);
+        }
+    }
+}
